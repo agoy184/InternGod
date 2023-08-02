48d2b1c2ffa10cbdc754276171cdf7b146bb5dec
Marshall <silvermercurius@outlook.com>
2023-08-02 15:26:40 -0700

    Added dialogue trigger

--- PlayerController.js
+++ PlayerController.js
@@ -1,31 +1,52 @@
 class PlayerController extends Phaser.Scene {
-    constructor() {
-      super("playGame");
-    }
+  constructor() {
+    super("playGame");
+  }
 
   preload() {
     this.load.image("bg_market", "assets/backgrounds/market1.png");
-
     this.load.image("intern", "assets/sprites/test_spr.png");
+    this.load.image("grandpa", "assets/sprites/grandpa-normal.png");
+    this.load.image("dialoguebox","assets/dialog_images/dialog_box.png");
   }
 
-  create()
-  {
-      this.background = this.add.image(0, 0, 'bg_market').setOrigin(0,0);
-  
-      this.background.displayWidth = this.sys.game.config.width;
-      this.background.displayHeight = this.sys.game.config.height;
-      
-      this.player = this.physics.add.sprite(this.game.config.width / 2, this.game.config.height /2 , "intern");
-      this.cursorKeys = this.input.keyboard.createCursorKeys();
-      this.player.setCollideWorldBounds(true);
+  create() {
+    this.Ekey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+    this.isOverlapping = false;
+
+    this.background = this.add.image(0, 0, 'bg_market').setOrigin(0,0);
+    this.background.displayWidth = this.sys.game.config.width;
+    this.background.displayHeight = this.sys.game.config.height;
+
+    this.player = this.physics.add.sprite(this.game.config.width / 2, this.game.config.height / 2 , "intern");
+    this.grandpa = this.physics.add.sprite(this.game.config.width / 4 , this.game.config.height / 4 , "grandpa").setScale(0.5);
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.player.setCollideWorldBounds(true);
+
+    this.npc = this.physics.add.group();
+    this.npc.add(this.grandpa);
+
+    this.physics.add.overlap(this.player, this.npc, this.Overlapped, null, this);
   }
-  
-  update(){
+
+  update() {
     this.movePlayerManager();
+
+    if (Phaser.Input.Keyboard.JustDown(this.Ekey) && this.isOverlapping) {
+      this.isOverlapping = false;
+      this.OpenDialogue();
+    }
   }
 
-  movePlayerManager(){
+  Overlapped(player, npc) {
+    this.isOverlapping = true;
+  }
+
+  OpenDialogue() {
+    this.dialogue = this.add.sprite(this.game.config.width / 2, this.game.config.height - 100, "dialoguebox").setScale(2.0);
+  }
+
+  movePlayerManager() {
     if (this.cursorKeys.left.isDown){
         this.player.setVelocityX(-gameSettings.playerSpeed);
     }
@@ -35,7 +56,7 @@ class PlayerController extends Phaser.Scene {
     else {
         this.player.setVelocityX(0);
     }
-    
+
     if(this.cursorKeys.up.isDown){
         this.player.setVelocityY(-gameSettings.playerSpeed);
     }
@@ -45,6 +66,5 @@ class PlayerController extends Phaser.Scene {
     else {
         this.player.setVelocityY(0);
     }
-}
-
+  }
 }
--- PreLoad.js
+++ PreLoad.js
@@ -7,6 +7,8 @@ class Load extends Phaser.Scene {
         this.load.image("bg_market", "assets/backgrounds/market1.png");
 
         this.load.image("intern", "assets/sprites/test_spr.png");
+
+        this.load.image("grandpa","assets/sprites/grandpa-normal.png")
     }
 
     
